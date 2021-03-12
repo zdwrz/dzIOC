93eec658a2626383c8cdac89371400c24be70dc5
Dawei Zhuang <dawei.zhuang@antra.com>
2021-03-12 14:05:21 -0500

    Updated: comments in the annotation

--- src/main/java/com/aweiz/dzaop/Advice.java
+++ src/main/java/com/aweiz/dzaop/Advice.java
@@ -3,6 +3,7 @@ package com.aweiz.dzaop;
 /**
  * Not functional. All are AROUND now.
  * Created by daweizhuang on 5/24/16.
+ * This is a test line
  */
 public enum Advice {
     BEFORE,AFTER,AROUND
